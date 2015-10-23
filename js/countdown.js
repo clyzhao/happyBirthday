fc6610a967e937c65a8f6ee1110f3164219140eb
yanghao <3024399@qq.com>
2015-10-23 21:10:29 +0800

    test

--- js/countdown.js
+++ js/countdown.js
@@ -4,7 +4,7 @@ var RADIUS = 8;
 var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 
-const endTime = new Date(2015,9,24,00,00,00);
+const endTime = new Date(2015,9,23,15,00,00);
 
 var curShowTimeSeconds = 0;
 var balls = [];
